c5be2efb535b00a9536e0120103041468bbf1563
anju-sagar <anju.sagar@telegraph.co.uk>
2019-07-19 22:34:44 +0100

    Calculator espresso tests

---
+++ app/src/androidTest/java/com/simplemobiletools/calculator/activities/AddNumbers.java
@@ -0,0 +1,118 @@
+package com.simplemobiletools.calculator.activities;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.simplemobiletools.calculator.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class AddNumbers {
+
+    @Rule
+    public ActivityTestRule<SplashActivity> mActivityTestRule = new ActivityTestRule<>(SplashActivity.class);
+
+    @Test
+    public void addNumbers() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_1), withText("1"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.calculator_holder),
+                                        5),
+                                0),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.btn_1), withText("1"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.calculator_holder),
+                                        5),
+                                0),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.btn_plus), withText("+"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.calculator_holder),
+                                        6),
+                                3),
+                        isDisplayed()));
+        appCompatButton3.perform(click());
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(R.id.btn_2), withText("2"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.calculator_holder),
+                                        5),
+                                1),
+                        isDisplayed()));
+        appCompatButton4.perform(click());
+
+        ViewInteraction appCompatButton5 = onView(
+                allOf(withId(R.id.btn_equals), withText("="),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.calculator_holder),
+                                        6),
+                                2),
+                        isDisplayed()));
+        appCompatButton5.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.result), withText("13"),
+                        childAtPosition(
+                                allOf(withId(R.id.calculator_holder),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        textView.check(matches(withText("13")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
